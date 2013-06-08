0efa254ed82099349891ae7bf53401167bdc693b
Piotr Gajos <piotr@sourcebits.com>
2013-06-07 21:24:43 -0700

    Modificationnos

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -278,6 +278,11 @@ defaults write com.apple.screencapture location -string "$HOME/Desktop"
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
 
+# Make menubar clock white
+open $HOME/Customizing/WhiteUIServer.menu
+
+# Restart Stuff
+
 for app in "Address Book" "Calendar" "Contacts" "Dashboard" "Dock" "Finder" \
 	"Mail" "Safari" "SystemUIServer" "Terminal" "Transmission" \
 	"Twitter" "iCal" "iTunes"; do
