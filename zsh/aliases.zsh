d528641645b8e93cca3be37e1ac78393da575351
Piotr Gajos <piotr@sourcebits.com>
2013-06-23 23:29:44 +0200

    Periodic update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,6 +2,6 @@ alias reload!='. ~/.zshrc'
 
 alias ls='ls -1a'
 alias cl='clear'
-alias code='cd ~/Code/Twine ; git pl ; git last'
+alias code='cd ~/Code/Twine-iOS ; git pl ; git last'
 alias markdownhelp='open http://j.mp/144z4kR'
 alias dirsizes='du -skh *(/)'
