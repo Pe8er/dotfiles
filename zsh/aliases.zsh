623ecd4241a67f7f438c5604386d9ea91cd81842
Piotr Gajos <piotr@sourcebits.com>
2013-11-06 16:36:48 +0100

    Updated 'code' script

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,7 +2,7 @@ alias reload!='. ~/.zshrc'
 
 alias ls='ls -1a'
 alias cl='clear'
-alias code='cd ~/Code/Twine-iOS ; git pl ; git last'
+alias code='cd ~/Code/Twine-iOS7 ; git pl ; git last ; git st'
 alias markdownhelp='open http://j.mp/144z4kR'
 alias dirsizes='du -skh *(/)'
 alias dotpull='cd ~/.dotfiles ; git pl'
