c81fe608123b6845eee14aae57fbe742f84bfeea
Piotr Gajos <piotr@sourcebits.com>
2014-11-16 18:45:00 -0800

    Added a brew

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -14,6 +14,6 @@ then
 fi
 
 # Install homebrew packages
-brew install grc coreutils spark xmlstarlet terminal-notifier node 
+brew install grc coreutils spark xmlstarlet terminal-notifier node switchaudio-osx
 
 exit 0
