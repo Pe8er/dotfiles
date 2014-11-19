25c77fa3d710726475775e272986e3d6a23bbc0e
Piotr Gajos <piotr@sourcebits.com>
2014-11-19 12:09:21 -0800

    Added JQ to brews, for weather widget

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -14,6 +14,6 @@ then
 fi
 
 # Install homebrew packages
-brew install grc coreutils spark xmlstarlet terminal-notifier node switchaudio-osx
+brew install grc coreutils spark xmlstarlet terminal-notifier node switchaudio-osx jq
 
 exit 0
