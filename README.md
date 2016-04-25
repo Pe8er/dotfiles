85701252947a3067083db958ba5fea5b2e9e1c00
Piotr Gajos <piotr@sourcebits.com>
2016-04-25 10:57:22 -0700

    Updated submodules

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+
+dotfiles
 # Piotr's dotfiles
 
 I don't know what I'm doing here. Forked from [this super awesome and shiny person](https://github.com/holman/dotfiles).
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,7 +2,6 @@ alias reload!='. ~/.zshrc'
 
 alias ll='ls -la'
 alias cl='clear'
-alias code='cd ~/Code/Twine ; git pl ; git last ; git st'
 alias markdownhelp='open http://j.mp/144z4kR'
 alias dirsizes='du -skh *(/)'
 alias dotpull='cd ~/.dotfiles ; git pl'
