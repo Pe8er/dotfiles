4df76e13c8f2f8cbc48ae5ec9ebf375816dfb9da
Piotr Gajos <pgajos@icloud.com>
2017-08-14 23:14:38 -0400

    old and new zsh

---
+++ zsh/aliases.zsh
@@ -0,0 +1,8 @@
+alias reload!='. ~/.zshrc'
+
+alias ll='ls -la'
+alias cl='clear'
+alias markdownhelp='open http://j.mp/144z4kR'
+alias dirsizes='du -skh *(/)'
+alias dotpull='cd ~/.dotfiles ; git pl'
+alias cls='clear'
