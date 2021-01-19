5bf514a0fe72905557127ae534fdfefc37a53729
Piotr Gajos <pgajos@icloud.com>
2021-01-19 10:45:26 -0800

    Switch and Karabiner

---
+++ library/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+ln -s $HOME/.dotfiles/library/LaunchAgents/com.pe8er.KeyRemapping.plist ~/Library/LaunchAgents/com.pe8er.KeyRemapping.plist
+
+sudo chown root:wheel ~/Library/LaunchAgents/com.pe8er.KeyRemapping.plist
