8f8fe8bd27709f4b3707eac67fc5ef292f7f1454
Piotr Gajos <pgajos@icloud.com>
2023-05-17 15:47:38 -0700

    New computer

--- library/~install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-ln -s $HOME/.dotfiles/library/LaunchAgents/com.pe8er.KeyRemapping.plist ~/Library/LaunchAgents/com.pe8er.KeyRemapping.plist
-
-sudo chown root:wheel ~/Library/LaunchAgents/com.pe8er.KeyRemapping.plist
--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,3 +1,5 @@
 export PATH="/usr/local/sbin:$PATH"
 export PATH="/usr/local/opt/ruby/bin:$PATH"
 export PATH="$HOME/.gem/ruby/2.7.0/bin:$PATH"
+export PATH="$HOME/.config/.spicetify:$PATH"
+export PATH="$HOME/.dotfiles/bin:$PATH"
