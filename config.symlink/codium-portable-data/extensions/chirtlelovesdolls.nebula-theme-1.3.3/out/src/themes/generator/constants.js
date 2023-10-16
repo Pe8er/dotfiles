4c669010e0580bfe1d2738943c97ca16c185ba61
Piotr Gajos <pgajos@icloud.com>
2023-10-16 21:59:27 +0200

    Test

--- config.symlink/codium-portable-data/extensions/chirtlelovesdolls.nebula-theme-1.3.3/out/src/themes/generator/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.lastNonConfigVer = exports.themeJsonName = void 0;
-/**
- * File name of the JSON file that will be generated to the out folder.
- */
-exports.themeJsonName = 'nebula-color-theme.json';
-exports.lastNonConfigVer = '1.0.5';
-//# sourceMappingURL=constants.js.map
